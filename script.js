4c7b34e2aeef7dce2ab4ff4e5fd652485fb30375
zemira kehati <kehati10@gmail.com>
2021-08-18 05:18:57 +0300

    project4 fix bem

--- script.js
+++ script.js
@@ -3,25 +3,24 @@ let profileTask = document.querySelector('.profile__task');
 let formName = document.querySelector('.form__item_name');
 let formAbout = document.querySelector('.form__item_about');
 let formElement = document.querySelector('.form');
-let formPopup = document.querySelector('.form__popup');
-let formPopupOpened = document.querySelector('.form__popup_opened');
-let elementNameHeart = document.querySelector('.element__name_heart')
+let Popup = document.querySelector('.popup');
+let PopupOpened = document.querySelector('.popup_opened');
 
 function openForm() {  
   formName.value = profileName.textContent;
   formAbout.value = profileTask.textContent;
-  formPopup.classList.add("form__popup_opened");
-  formPopupOpened.setAttribute("style", "display:block");
+  Popup.classList.add("popup_opened");
+  PopupOpened.setAttribute("style", "display:block");
 }
 
 function closeForm() {  
-  if (formPopup.classList.contains("form__popup_opened")){
-        formPopup.classList.remove("form__popup_opened");
-        formPopup.setAttribute("style", "display:none");
+  if (Popup.classList.contains("popup_opened")){
+        Popup.classList.remove("popup_opened");
+        Popup.setAttribute("style", "display:none");
   }
   else {
-       formPopup.classList.add("form__popup_opened");
-       formPopupOpened.setAttribute("style", "display:block");
+       Popup.classList.add("popup_opened");
+       PopupOpened.setAttribute("style", "display:block");
   }
 }
 
